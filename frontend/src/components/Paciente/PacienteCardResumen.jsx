e5efe467501476eb9cb0d995142fb88d3c9ede0a
KeilaCruz <zS20019068@estudiantes.uv.mx>
2024-03-21 14:57:22 -0600

    Primer version calendario paciente

--- frontend/src/components/Paciente/PacienteCardResumen.jsx
+++ frontend/src/components/Paciente/PacienteCardResumen.jsx
@@ -2,8 +2,11 @@ import { useNavigate } from "react-router-dom"
 
 export function PacienteCardResumen({ paciente }) {
     const navigate = useNavigate("")
-    const handleNavigate = async () => {
+    const handleNavigateFicha = async () => {
         navigate(`/buscar_paciente/${paciente.CURP}`)
+    }
+    const handleNavigateCitas = async () => {
+        navigate(`/citas_paciente/${paciente.CURP}`)
     }
     return (
         <>
@@ -13,7 +16,8 @@ export function PacienteCardResumen({ paciente }) {
                         <p className="cards-buscar-text text-center">{paciente.CURP}</p>
                         <p className="cards-buscar-text text-center">{paciente.datos_personales.nombre} {paciente.datos_personales.apePaterno} {paciente.datos_personales.apeMaterno}</p>
                         <p className="cards-buscar-text text-center">{paciente.datos_contacto.telefono}</p>
-                        <button onClick={handleNavigate}>Ver</button>
+                        <button onClick={handleNavigateFicha}>Ver</button>
+                        <button onClick={handleNavigateCitas}>Citas</button>
                     </div>
 
                 </div>
